7e5e7e9edc1125a336b6ef45ae4264a2e74bfe0b
Mario Bonaldi <mario.bonaldi@gmail.com>
2021-08-02 15:42:44 -0300

    test: ensure EmailValidatorAdapter returns false if validator resturns false

--- src/utils/email-validator.ts
+++ src/utils/email-validator.ts
@@ -1,6 +1,6 @@
-import {} from '../presentation/protocols/email-validator'
+import { EmailValidator } from '../presentation/protocols/email-validator'
 
-export class EmailValidatorAdapter {
+export class EmailValidatorAdapter implements EmailValidator {
   isValid (email: string): boolean {
     return false
   }
