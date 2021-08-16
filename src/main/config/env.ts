ce5a195156f80e5a9fb12fd91787f32d37b3154d
Mario Bonaldi <mario.bonaldi@gmail.com>
2021-08-16 17:09:53 -0300

    chore: change mongodb url

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,4 @@
 export default {
-  mongoUrl: global.__MONGO_URI__ ?? 'mongodb://localhost:27017/node-course',
+  mongoUrl: global.__MONGO_URI__ ?? 'mongodb://balta:e296cd9f@localhost:27017/admin',
   port: process.env.PORT ?? 5050
 }
