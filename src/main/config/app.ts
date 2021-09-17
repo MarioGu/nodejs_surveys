6d91aa97ae2fb8ea1245e56424e3e44b2a6937d9
Mario Bonaldi <mario.bonaldi@gmail.com>
2021-09-17 11:32:27 -0300

    Revert "Revert "Revert "Revert "Revert "chore: remove dotenv package"""""
    This reverts commit 5344c5b5825de02f498d5a1bd269c8c187801d36.

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,9 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 setupMiddlewares(app)
